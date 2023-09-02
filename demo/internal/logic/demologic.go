87977f4a50225bcfbe448fa547ebba094cd67bc0
xyl <416319808@qq.com>
2023-09-02 23:18:15 +0800

    demo

---
+++ demo/internal/logic/demologic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"zero/demo/internal/svc"
+	"zero/demo/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type DemoLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewDemoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DemoLogic {
+	return &DemoLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *DemoLogic) Demo(req *types.Request) (resp *types.Response, err error) {
+	resp = new(types.Response)
+	resp.Message = req.Name
+	return
+}
