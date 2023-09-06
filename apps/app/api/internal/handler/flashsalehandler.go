543f83aecbca70aa003d424cdec765a6933ffb55
xyl <416319808@qq.com>
2023-09-06 23:06:27 +0800

    demo代码

---
+++ apps/app/api/internal/handler/flashsalehandler.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"zero/apps/app/api/internal/logic"
+	"zero/apps/app/api/internal/svc"
+)
+
+func FlashSaleHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		l := logic.NewFlashSaleLogic(r.Context(), svcCtx)
+		resp, err := l.FlashSale()
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
