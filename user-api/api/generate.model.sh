9115fdcfd88382c5e3d36f22cb822ff33f64737c
xyl <416319808@qq.com>
2023-09-11 17:33:03 +0800

    列表查询

--- user-api/api/generate.model.sh
+++ user-api/api/generate.model.sh
@@ -15,4 +15,4 @@ username=root
 passwd=123456
 
 echo "开始创建库：$dbname 的表：$2"
-goctl model mysql datasource -url="${username}:${passwd}@tcp(${host}:${port})/${dbname}" -table="${table}" -dir="${modeldir}" --style=goZero
+goctl model mysql datasource -url="${username}:${passwd}@tcp(${host}:${port})/${dbname}" -table="${table}" -dir="${modeldir}" --style=goZero --cache=true
